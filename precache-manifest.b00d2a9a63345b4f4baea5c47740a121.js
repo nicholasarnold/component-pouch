d540c2e0512c0497147b02c4e102b9f9b62f782a
Nick Arnold <nicholasarnold@users.noreply.github.com>
2019-03-14 16:59:24 -0400

    Updates

---
+++ precache-manifest.b00d2a9a63345b4f4baea5c47740a121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0eac654aa7cd37ccfb39dca9a6176397",
+    "url": "/component-pouch/static/media/minus-button.0eac654a.svg"
+  },
+  {
+    "revision": "e93377be679220ff2ac6f94dca664497",
+    "url": "/component-pouch/static/media/add-button.e93377be.svg"
+  },
+  {
+    "revision": "c9299c785095a49232b3",
+    "url": "/component-pouch/static/js/runtime~main.c2731f80.js"
+  },
+  {
+    "revision": "17d198d9a179389eb413",
+    "url": "/component-pouch/static/js/main.dd20c577.chunk.js"
+  },
+  {
+    "revision": "f733cd8443e37305289a",
+    "url": "/component-pouch/static/js/2.c6ac93ca.chunk.js"
+  },
+  {
+    "revision": "17d198d9a179389eb413",
+    "url": "/component-pouch/static/css/main.3be1404c.chunk.css"
+  },
+  {
+    "revision": "495034625ff252a3a4f2815fc6c992f2",
+    "url": "/component-pouch/index.html"
+  }
+];
